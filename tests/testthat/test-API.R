e1dcc88fa2d5112f29043997ecd72f50aeb4b0c9
jgilber2 <JGIlber2@its.jnj.com>
2021-06-23 14:38:55 -0700

    tests for api loading

--- tests/testthat/test-API.R
+++ tests/testthat/test-API.R
@@ -1,3 +1,17 @@
+test_that("load function works", {
+  tenv <- environment()
+  api <- CemConnector::loadApi(connectionDetails,
+                               cemSchema = cemTestSchema,
+                               vocabularySchema = vocabularySchema,
+                               sourceSchema = sourceInfoSchema,
+                               envir = tenv)
+  api$setDocs(FALSE)
+  expect_s3_class(api, "Plumber")
+
+  # Test load endpoints
+  expect_class(tenv$cemBackendApi, "CemDatabaseBackend")
+  tenv$cemBackendApi$finalize()
+})
 
 test_that("Test api alive", {
   skip_if_not(useTestPlumber, "Test plumber not loaded")
@@ -19,7 +33,6 @@ test_that("Test get source info", {
   content <- httr::content(resp, as = "parsed")
   expect_true(length(content$result) > 5)
 })
-
 
 test_that("Test get condition evidence", {
   skip_if_not(useTestPlumber, "Test plumber not loaded")
@@ -51,7 +64,7 @@ test_that("Test get ingredient evidence", {
 
 test_that("Test get relationships", {
   skip_if_not(useTestPlumber, "Test plumber not loaded")
-  srchIngredientSet <- data.frame(conceptId = c(21604296, 1201620), includeDescendants = c(1,0), isExcluded = c(0))
+  srchIngredientSet <- data.frame(conceptId = c(21604296, 1201620), includeDescendants = c(1, 0), isExcluded = c(0))
   srchOutcomeConceptSet <- data.frame(conceptId = c(4149320), includeDescendants = c(1), isExcluded = c(0))
 
   resp <- httr::POST(paste0(apiUrl, "/ingredientEvidenceSummary"),
