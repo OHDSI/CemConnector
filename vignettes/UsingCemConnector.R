d2d7c165123d41d318929dbc44f4c0a1485b672c
jgilber2 <JGIlber2@its.jnj.com>
2021-07-27 11:45:37 -0700

    Updates to docs

--- vignettes/UsingCemConnector.R
+++ vignettes/UsingCemConnector.R
@@ -1,12 +1,20 @@
-## ---- include = FALSE---------------------------------------------------------
+## ---- echo = FALSE, message = FALSE, warning = FALSE--------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
+devtools::load_all()
+sqlidb <- tempfile(fileext = ".sqlite")
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms="sqlite", server = sqlidb)
 
-## ---- eval = FALSE------------------------------------------------------------
-#  baseUrl <- "https://cem.ohdsi.org/api/v1/"
-#  cemConnection <- CemConnector::CemWebApiBackend$new(baseUrl)
+.loadCemTestFixtures(connectionDetails)
+
+## -----------------------------------------------------------------------------
+
+cemConnection <- CemConnector::CemDatabaseBackend$new(connectionDetails = connectionDetails,
+                                                      vocabularySchema = "main",
+                                                      cemSchema = "main",
+                                                      sourceSchema = "main")
 
 ## -----------------------------------------------------------------------------
 ingreditentConceptSet <- data.frame(conceptId = c(1201620), includeDescendants = c(0), isExcluded = c(0))
@@ -14,7 +22,14 @@ ingreditentConceptSet <- data.frame(conceptId = c(1201620), includeDescendants =
 ## -----------------------------------------------------------------------------
 parentIngredientSet <- data.frame(conceptId = c(21604296), includeDescendants = c(1), isExcluded = c(0))
 
-## ---- eval = FALSE------------------------------------------------------------
-#  outcomeConcepts <- backend$getIngredientEvidenceSummary(ingreditentConceptSet)
-#  outcomeConcepts
+## -----------------------------------------------------------------------------
+outcomeConcepts <- cemConnection$getIngredientEvidenceSummary(ingreditentConceptSet)
+outcomeConcepts
+
+## -----------------------------------------------------------------------------
+outcomeConcepts <- cemConnection$getIngredientEvidenceSummary(ingreditentConceptSet)
+
+## -----------------------------------------------------------------------------
+outcomeConcepts <- cemConnection$getSuggestedControlCondtions(ingreditentConceptSet)
+outcomeConcepts
 
