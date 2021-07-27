90486f84c0745d1c3ba9528d11bc3efd89c92df1
jgilber2 <JGIlber2@its.jnj.com>
2021-07-27 11:50:09 -0700

    Updates to docs

--- vignettes/UsingCemConnector.R
+++ vignettes/UsingCemConnector.R
@@ -5,16 +5,28 @@ knitr::opts_chunk$set(
 )
 devtools::load_all()
 sqlidb <- tempfile(fileext = ".sqlite")
-connectionDetails <- DatabaseConnector::createConnectionDetails(dbms="sqlite", server = sqlidb)
-
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "sqlite", server = sqlidb)
 .loadCemTestFixtures(connectionDetails)
-
-## -----------------------------------------------------------------------------
-
 cemConnection <- CemConnector::CemDatabaseBackend$new(connectionDetails = connectionDetails,
                                                       vocabularySchema = "main",
                                                       cemSchema = "main",
                                                       sourceSchema = "main")
+
+## ---- eval = FALSE------------------------------------------------------------
+#  
+#  cemConnection <- CemConnector::CemWebApiBackend(apiUrl = "https://cem.ohdsi.org/")
+
+## ---- eval = FALSE------------------------------------------------------------
+#  
+#  connectionDetails <- DatabaseConnector::createConnectionDetails(user = "mydbusername",
+#                                                                  server = "myserver/foo",
+#                                                                  dbms = "redshift",
+#                                                                  password = "mysecret")
+#  
+#  cemConnection <- CemConnector::CemDatabaseBackend$new(connectionDetails = connectionDetails,
+#                                                        vocabularySchema = "vocabulary",
+#                                                        cemSchema = "cem_v2",
+#                                                        sourceSchema = "cem_v2_source")
 
 ## -----------------------------------------------------------------------------
 ingreditentConceptSet <- data.frame(conceptId = c(1201620), includeDescendants = c(0), isExcluded = c(0))
